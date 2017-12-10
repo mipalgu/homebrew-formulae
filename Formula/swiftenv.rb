e59ff6ca619f84b8984a8c502dd1ba946662d47c
Rene Hexel <rhx@users.noreply.github.com>
2017-12-10 11:32:11 +1000

    Upgrade swiftenv to 1.4.2

--- Formula/swiftenv.rb
+++ Formula/swiftenv.rb
@@ -1,8 +1,8 @@
 class Swiftenv < Formula
   desc "Swift version manager"
   homepage "https://github.com/rhx/swiftenv"
-  url "https://github.com/rhx/swiftenv/archive/1.4.1.tar.gz"
-  sha256 "2b96332db0de8bf17e35021c3feaac26e1e62b9bbc09f074a47716321b178b11"
+  url "https://github.com/rhx/swiftenv/archive/1.4.2.tar.gz"
+  sha256 "ab19ae8b5a9fc5073d82f9aa0682b4bad21858f6ba313def13dc706ce4b4f019"
   head "https://github.com/rhx/swiftenv.git"
 
   def install
